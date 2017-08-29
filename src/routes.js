f9d1340c2356294267639a5c62467e41acf9f1c9
Schuster <Peter_Schuster@cable.comcast.com>
2017-08-29 09:50:00 -0400

    adds typography

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import Home from './containers/Home'
 import Colors from './containers/Base/Colors'
+import Typography from './containers/Base/Typography'
 
 export default <Route path="/" component={App}>
   <IndexRoute component={Home} />
   <Route path="/base/colors" component={Colors} />
+  <Route path="/base/typography" component={Typography} />
 </Route>
