3573c34dbc466197fb7964ac395cdfd0e5745a0b
Schuster <Peter_Schuster@cable.comcast.com>
2017-09-11 15:38:18 -0400

    update actions

--- src/containers/Components/Action.js
+++ src/containers/Components/Action.js
@@ -5,7 +5,7 @@ class Buttons extends Component {
   render() {
     return (
       <PageSection>
-        <h1>Loading</h1>
+        <h1>Actions</h1>
         <hr />
 
         <h2>Default Action</h2>
@@ -25,6 +25,16 @@ class Buttons extends Component {
 
         <h2>Multi Action Right</h2>
         <div class="action action--right">
+          <div class="action__item">
+            <a class="button button--primary">Button Text</a>
+          </div>
+          <div class="action__item">
+            <a class="button">Button Text</a>
+          </div>
+        </div>
+
+        <h2>Multi Action Primary Right but Centered</h2>
+        <div class="action action--right-centered">
           <div class="action__item">
             <a class="button button--primary">Button Text</a>
           </div>
